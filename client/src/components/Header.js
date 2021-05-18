8013a173cd9d64cd6a5d61e039acee2495d6abc5
jyguzman <jordieyguzman@gmail.com>
2021-05-17 20:06:23 -0700

    Made header & dog gallery mobile-friendly

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,10 +4,8 @@ import Container from '@material-ui/core/Container';
 const Header = () => {
 	return (
 		<Container className="color">
-			<div className="header">
-					<h1>The Dog Repawsitory</h1>	
-					<img src={"dog.jpg"} alt="Dog"/>
-			</div>
+				<h1>The Dog Repawsitory</h1>	
+				<img className = "headerImg" src={"dog.jpg"} alt="Dog"/>
 		</Container>
 	);
 };
