d9d029fa3e69502b0790a8c111e7d34cb0d29c00
jyguzman <jordieyguzman@gmail.com>
2021-05-23 16:17:45 -0700

    Update gitignore

--- server.js
+++ server.js
@@ -72,11 +72,11 @@ app.get("/top", (req, res) => {
 	mongoClient.close();
 });
 
-/*app.use(express.static(path.resolve(__dirname, 'client/build')));
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-});*/
+});
 
 app.listen(PORT, () => {
 	console.log("Server listening on " + PORT);
